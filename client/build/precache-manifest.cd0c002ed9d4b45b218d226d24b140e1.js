abdbd8a653fce5b714c06ab81b1040bfb154028a
Rahul Prajapati <rahulprajapat41437@gmail.com>
2022-07-06 01:21:04 +0530

    commit build version

---
+++ client/build/precache-manifest.cd0c002ed9d4b45b218d226d24b140e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa8f3429e6800c22b81e3311b975035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3985ce5ee3ceabfbcd79",
+    "url": "/static/css/main.221c38a6.chunk.css"
+  },
+  {
+    "revision": "a80f3e6f9c305d15b294",
+    "url": "/static/js/2.17f6f61e.chunk.js"
+  },
+  {
+    "revision": "dba0c006430f86ab5dfd81528eb42c84",
+    "url": "/static/js/2.17f6f61e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3985ce5ee3ceabfbcd79",
+    "url": "/static/js/main.f0c662b7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
